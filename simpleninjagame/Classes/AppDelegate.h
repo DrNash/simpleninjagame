6b40283482faecbee99b7c10d827ade8503f205b
Ryan Nash <nash@gust.com>
2014-06-30 19:37:36 -0400

    Initial Commit

---
+++ simpleninjagame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  simpleninjagame
+//
+//  Created by gust on 6/30/14.
+//  Copyright Ryan Lurfs You 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ simpleninjagame/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  simpleninjagame
+//
+//  Created by gust on 6/30/14.
+//  Copyright Ryan Lurfs You 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
